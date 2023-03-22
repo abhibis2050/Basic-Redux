58c1a0a223430400c7373cb0253e2be5d01b9570
abhibis2050 <abhisekbiswas2050@gmail.com>
2023-03-23 02:04:26 +0530

    Bio data two fourth commit

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,18 +1,26 @@
 import React from 'react'
 import { useDispatch,useSelector } from 'react-redux'
-import { updateAge, updateName, updateStatus } from '../reducers/userReducer'
+import { fetchUserName, updateAge, updateName, updateStatus } from '../reducers/userReducer'
 
 
 const Profile = () => {
 const {name,age,status}=useSelector((state)=>state.user)
 const dispatch = useDispatch()
 
+const changeName=async(name)=>{
+  
+
+  dispatch(fetchUserName())
+}
+
+
+
   return (
     <div>
       <h1>i am {name}</h1>
       <h2>My Age Is {age}</h2>
       <h2>I Am A  {status}</h2>
-      <button onClick={()=>dispatch(updateName("Rahul"))}>Change Name</button>
+      <button onClick={()=>changeName("Rahul")}>Change Name</button>
       <button onClick={()=>dispatch(updateStatus("Graphic Desighner"))}>Change status</button>
       <button onClick={()=>dispatch(updateAge(26))}>Change Age</button>
     </div>
--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -1,10 +1,23 @@
-import {createSlice} from "@reduxjs/toolkit";
+import {createSlice,createAsyncThunk} from "@reduxjs/toolkit";
 
 const initialState={
     name:"Abhisek",
     age:20,
     status:"Web Developer"
 }
+
+
+
+export const fetchUserName = createAsyncThunk(
+    'getUserName',
+    async () => {
+        const res = await fetch("https://jsonplaceholder.typicode.com/users")
+        const response = await res.json()
+        const name2 = response[0].name
+        return name2
+    }
+   
+  )
 
 export const userReducer =  createSlice({
     name :"person",

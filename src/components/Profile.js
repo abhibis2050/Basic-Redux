01da997e9dc488ac046e92956ccc9cc0491ab946
abhibis2050 <abhisekbiswas2050@gmail.com>
2023-03-22 23:58:26 +0530

    Bio Data Two commit three

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,35 +1,20 @@
 import React from 'react'
 import { useDispatch,useSelector } from 'react-redux'
-import {updateStatusAction,fetchName} from  "../actions"
+import { updateAge, updateName, updateStatus } from '../reducers/userReducer'
 
 
 const Profile = () => {
 const {name,age,status}=useSelector((state)=>state.user)
 const dispatch = useDispatch()
 
-
-const updateStatus=(status)=>{
-    dispatch(updateStatusAction(status))
-}
-
-const updateAge=(age)=>{
-    dispatch({
-        type:" ",
-        payload:age
-    })
-}
-
-const updateName=async ()=>{
-       dispatch(fetchName())
-}
   return (
     <div>
       <h1>i am {name}</h1>
-      {/* <h2>My Age Is {age}</h2> */}
-      {/* <h2>I Am A  {status}</h2> */}
-      <button onClick={()=>updateName()}>Change Name</button>
-      <button onClick={()=>updateStatus("Designer")}>Change status</button>
-      <button onClick={()=>updateAge(30)}>Change Age</button>
+      <h2>My Age Is {age}</h2>
+      <h2>I Am A  {status}</h2>
+      <button onClick={()=>dispatch(updateName("Rahul"))}>Change Name</button>
+      <button onClick={()=>dispatch(updateStatus("Graphic Desighner"))}>Change status</button>
+      <button onClick={()=>dispatch(updateAge(26))}>Change Age</button>
     </div>
   )
 }
--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -1,5 +1,4 @@
 import {createSlice} from "@reduxjs/toolkit";
-import { updateStatusAction } from "../actions";
 
 const initialState={
     name:"Abhisek",
@@ -11,17 +10,17 @@ export const userReducer =  createSlice({
     name :"person",
     initialState,
     reducers:{
-        UPDATE_NAME(state,action){
+        updateName(state,action){
             state.name=action.payload
         },
-        UPDATE_AGE(state,action){
+        updateAge(state,action){
             state.age=action.payload
         },
-        UPDATE_STATUS(state,action){
+        updateStatus(state,action){
             state.status=action.payload
         }
     }
 })
 
-export const {UPDATE_NAME,UPDATE_AGE,UPDATE_STATUS} = userReducer.actions
+export const {updateName,updateAge,updateStatus} = userReducer.actions
 export default userReducer.reducer

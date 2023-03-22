2bfe38ca2362c118930f8276f7786e16f17922cd
drivadosayan <techsupport1@drivado.com>
2023-03-22 19:50:57 +0530

    BioData Two Commit One

--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -1,4 +1,4 @@
-import { createReducer } from "@reduxjs/toolkit";
+import {createSlice} from "@reduxjs/toolkit";
 import { updateStatusAction } from "../actions";
 
 const initialState={
@@ -7,23 +7,10 @@ const initialState={
     status:"Web Developer"
 }
 
-export default createReducer(initialState,(builder)=>{
-
-    // builder.addCase("UPDATE_STATUS",(state,action)=>{
-    //     state.status=action.payload
-    // })
-
-    builder.addCase("UPDATE_AGE",(state,action)=>{
-        state.age=action.payload
-    })
-
-    //reusing the component of action 
-    builder.addCase(updateStatusAction,(state,action)=>{
-        state.status=action.payload
-    })
-
-    
-    builder.addCase("UPDATE_NAME",(state,action)=>{
-        state.name=action.payload
-    })
+export default createSlice({
+    name :"person",
+    initialState,
+    reducers:{
+        
+    }
 })

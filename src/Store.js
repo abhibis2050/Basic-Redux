651e3b655edfd30d4ae5449138a28a3ba4d296d0
drivadosayan <techsupport1@drivado.com>
2023-03-25 16:13:44 +0530

    BioData With redux Saga

--- src/Store.js
+++ src/Store.js
@@ -1,9 +1,17 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore} from "@reduxjs/toolkit";
+import createSagaMiddleware from 'redux-saga'
 import userReducer from "./reducers/userReducer";
+import mySaga from "./sagas/userSaga";
 
+// create the saga middleware
+const sagaMiddleware = createSagaMiddleware()
 
 export const Store =configureStore ({
 reducer:{
 user:userReducer
-}
+},
+middleware:(getDefaultMiddleware)=>[...getDefaultMiddleware(),sagaMiddleware]
 })
+
+// then run the saga
+sagaMiddleware.run(mySaga)

351209a277cf3fd401743b5aae3c7f74b97d5a5c
drivadosayan <techsupport1@drivado.com>
2023-04-03 11:05:12 +0530

    tailwind commit two

--- src/components/ComponentOne.js
+++ src/components/ComponentOne.js
@@ -1,11 +1,17 @@
 import React from "react";
-
+import bgImage from "../assets/1.jpg";
 const ComponentOne = () => {
   return (
     <div>
-      <div className="h-screen justify-center items-center flex">
-        <div
-          className="w-2/5 h-1/5
+      <div className="flex">
+
+        <div className="w-1/2 border-4 border-black">
+          <img src={"https://images.pexels.com/photos/4195324/pexels-photo-4195324.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"} alt="" />
+        </div>
+       
+        <div className="h-screen justify-center items-center flex w-1/2 border-4 border-black">
+          <div
+            className="w-2/5 h-1/5
           xs:w-2/5
           sm:w-3/5
           bg-red-900
@@ -16,10 +22,10 @@ const ComponentOne = () => {
            xl:bg-purple-950
            2xl:bg-black
            "
-           
-        ></div>
+          >
+          </div>
+        </div>
       </div>
-      <button className="btn-primary">add button</button>
     </div>
   );
 };
